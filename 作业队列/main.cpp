779d32bedd5c8a8f0c437de62b394bcda89b867e
20181102931 <18647197975@163.com>
2019-11-05 16:12:39 +0800

    Initial Commit

---
+++ 作业队列/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  作业队列
+//
+//  Created by s20181102931 on 2019/11/5.
+//  Copyright © 2019 s20181102931. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
